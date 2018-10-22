6522c7a548a0789373c248f06a5aca63df949ce2
Jason Lee <huacnlee@gmail.com>
2018-10-22 14:42:46 +0800

    Add test case for field: :email

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   form_select :login, field: [:login, :to_param], scope: -> { order("id desc") }
   form_select :email
-  form_select :email_value, field: [:email]
+  form_select :email_value, field: :email
 
   def to_param
     login.downcase
